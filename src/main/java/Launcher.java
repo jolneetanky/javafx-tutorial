77e7d738bcddab67e5e192622dfed410b2d1a869
jolene <jolenetandev@gmail.com>
2026-09-03 00:38:11 +0800

    level one

---
+++ src/main/java/Launcher.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+
+/**
+ * Launches the JavaFX application.
+ */
+public class Launcher {
+
+    /**
+     * Starts the Hello World JavaFX application.
+     *
+     * @param args command line arguments passed to JavaFX
+     */
+    public static void main(String[] args) {
+        Application.launch(Main.class, args);
+    }
+}
